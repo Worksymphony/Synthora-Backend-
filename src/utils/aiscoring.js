76a25ee3213d10d8aa7ff6d34d493194ef34ae94
Shaikh Amaan <113681911+joyboy7867@users.noreply.github.com>
2025-09-26 17:55:54 +0530

    Update aiscoring.js
    change model to 2.5 flash-lite

--- src/utils/aiscoring.js
+++ src/utils/aiscoring.js
@@ -37,7 +37,7 @@ ${resumeText}
     if (process.env.GEMINI_API_KEY) {
       console.log("Trying Gemini...");
       const geminiRes = await axios.post(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${process.env.GEMINI_API_KEY}`,
         { contents: [{ parts: [{ text: prompt }] }] }
       );
       const geminiText = geminiRes.data.candidates?.[0]?.content?.parts?.[0]?.text;
